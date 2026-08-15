b486047ddc0fe06e693f6c45781c288a6d4141d5
umakantchauhan <140892351+umakantchauhan@users.noreply.github.com>
2026-08-15 18:43:10 +0530

    added the google analytics code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/Provider";
@@ -28,6 +29,21 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZSM8SCK3Z1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZSM8SCK3Z1');
+          `}
+        </Script>
+      </head>
+
       <body className="min-h-full flex flex-col">
         <ThemeProvider
           attribute="class"
